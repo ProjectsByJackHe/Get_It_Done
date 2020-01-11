8f0403f21d24f6dbc97d02bd174378ae5d9e97f3
Jack He <jackhe2345@gmail.com>
2020-01-11 13:18:04 -0800

    Initial Commit

---
+++ Get_It_Done/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get_It_Done
+//
+//  Created by Jack He on 1/11/20.
+//  Copyright © 2020 Jack He Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
