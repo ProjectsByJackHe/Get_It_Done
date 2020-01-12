ec53de856386e69cb46ffa03d4f224b5791efe7e
Phillbert Tan <3781117+phillytan@users.noreply.github.com>
2020-01-12 12:52:26 -0800

    Requests

--- Get_It_Done/UserProfile/UserProfileViewController.swift
+++ Get_It_Done/UserProfile/UserProfileViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class UserProfileViewController: UIViewController {
 
@@ -30,12 +31,38 @@ class UserProfileViewController: UIViewController {
     @IBOutlet weak var skill2Display: UILabel!
     @IBOutlet weak var skill3Display: UILabel!
     
+    let hostname = "https://api.getitdonetoday.online";
+    
+    let token = UserDefaults.standard.string(forKey: "isLogged")
+    
     
     
     @IBAction func requestJob(_ sender: UIButton) {
         let alert = UIAlertController(title: "Job query", message: "Please enter details regarding the job", preferredStyle: UIAlertController.Style.alert)
         alert.addTextField()
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {(action) in
+            struct User: Encodable {
+                let user: String
+            }
+            let headers: HTTPHeaders = [
+                "Authorization": "Bearer "+self.token!,
+                "Accept": "application/json"
+            ]
+            
+            let user = User(user: self.curEmail.text ?? "");
+            
+            AF.request(self.hostname+"/auth/sendrequests",
+                       method: .post,
+                       parameters: user,
+                       encoder: JSONParameterEncoder.default,
+                       headers: headers).responseJSON {response in
+                            let alert = UIAlertController(title: "Requested", message: "Requested", preferredStyle: UIAlertController.Style.alert)
+                            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {(action) in alert.dismiss(animated: true, completion: nil)}))
+                            self.present(alert, animated: true, completion: nil)
+                            print("Requested")
+            };
+            alert.dismiss(animated: true, completion: nil)
+        }))
         self.present(alert, animated: true, completion: nil)
     }
     
